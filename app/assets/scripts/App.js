05d8898a30d79fc2833369f3336ba98a37e280d9
marescanog <marescanog@gmail.com>
2020-12-20 17:24:33 +0800

    Added 4 other HTML Pages

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,17 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 70);
+var homeUrl = 'index.html';
+if ( window.location.href.indexOf(homeUrl) > 0 ){
+    new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+    new RevealOnScroll(document.querySelectorAll(".testimonial"), 70);
+}
+
+let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenu();
+
 
 if (module.hot) {
     module.hot.accept()
